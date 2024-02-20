87a78f6e75461f339b153b63e60940a2d49a1b2a
Andrei Stepanov <steam.oni.am@gmail.com>
2024-02-20 23:38:59 +0400

    +Singleton

---
+++ Creational/FactoryMethod/Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread otherThread = new Thread(() =>
+            {
+                Console.WriteLine(Singleton.Instance().DateTime);
+            });
+            otherThread.Start();
+
+            Console.WriteLine(Singleton.Instance().DateTime);
+        }
+
+        public class Singleton
+        {
+            private static Singleton _singleton = null;
+            private static object _lock = new object();
+
+            public DateTime DateTime { get; private set; }
+            private Singleton()
+            {
+                DateTime = DateTime.Now;
+            }
+
+            public static Singleton Instance()
+            {
+                if (_singleton == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_singleton == null)
+                        {
+                            _singleton = new Singleton();
+                        }
+                    }
+                }
+
+                return _singleton;
+            }
+        }
+    }
+}
